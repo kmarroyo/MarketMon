c8cc7e9dc7a274d333cb110aba24766552e10696
Keith Arroyo <keitharroyo@gmail.com>
2019-01-29 17:03:57 -0600

    1-29-19

--- marketmon/marketmon.py
+++ marketmon/marketmon.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3.5
 import json, requests
 import tkinter as tki   #gui
 import tkinter.font  #gui
 import threading
+import socket
 
 print ('hello world')
 #url = requests.get('https://api.iextrading.com/1.0/stock/aapl/book')
@@ -35,9 +37,13 @@ labelLeftTop.pack()
 #labelLeftMid = tki.Label(win, width="10", height = "3",bg = "midnight blue")
 #labelLeftBot = tki.Label(win, width="10", height = "3",bg = "blue")
 labelRightTop = tki.Label(win,justify="right", width="7", height = "3",bg = "#001F3F",fg = "OliveDrab1")
-#labelRightTop.pack()
+
 #labelRightMid = tki.Label(win, width="10", height = "3",bg = "midnight blue")
 #labelRightBot = tki.Label(win, width="10", height = "3",bg = "midnight blue")
+
+#labelLeftTop.grid_propagate(True)
+#labelRightTop.grid_propagate(True)
+
 
 labelLeftTop.grid(row =0 ,column=0,sticky='nsew')
 #labelLeftMid.grid(row =1 ,column=0,sticky='nsew')
@@ -56,55 +62,192 @@ win.grid_rowconfigure(0,weight=1)
 
 labelLeftTop["text"] = "1234567\n45\n1234567"
 labelRightTop["text"] = "1234567\n45\n1234567"
-
+oldHeight = 0
 def resize_lt(event):
     print("In lt",event.height, event.x, event.y)
-    global FirstTimeInresize   
+    global FirstTimeInresize
+    global oldHeight
     if FirstTimeInresize == False:
+
         myFont['size'] = round(event.height/(5))
         print(round(event.height/(5)))
-        labelLeftTop['font'] = myFont
-        labelRightTop['font'] = myFont
+        if round(event.height/(5)) != oldHeight:
+            
+            oldHeight = round(event.height/(5))
+            labelLeftTop['font'] = myFont
+            labelRightTop['font'] = myFont
+        
+        
     else:
+        oldHeight = round(event.height/(5))
         FirstTimeInresize = False
 FirstTimeInresize = True # For some reason, first time threw causes main window to grow past physical screen size        
+sName = "SPY"
+stockName = "" 
+def keystroke(event):
+    global stockName
+    global sName
+    if len(event.char) == 1:
+        
+        print (ord(event.char))
+        if ord(event.char) == 13:
+            print ("Enter Key pressed")
+            print (stockName)
+            sName = stockName
+            stockName = ""
+        else:
+            if len(stockName) < 6:
+                stockName += event.char
 
+win.bind("<Key>", keystroke)
 
-def TimerPopped():
+NORMAL = 0
+ISSUED_CLOSE = 1
+stateUDP = NORMAL
+
+prevtimerCount = 0
+def DeadManTimer():
+    global dtimer
     global timer
-#    print ('refreshing')
-    url = requests.get('https://api.iextrading.com/1.0/stock/ibm/quote')
-    text = json.loads(url.text)
-
-#    labelLeftTop["text"] = "1234567\n45\n1234567"
-    quote = text['iexRealtimePrice']
-    quoteStr = str(quote)
-#    print (len(quoteStr))
-    if (len(quoteStr) > 1) and (len(quoteStr) < 8):
-        labelRightTop["text"] = quoteStr + "\n\n"
+    global prevtimerCount
+    global  serverSock
+    global ReadUDP
+    global UDP_IP_ADDRESS
+    global stateUDP
+    global ISSUED_CLOSE
+    global NORMAL
     
-    if (len(text['symbol']) > 1) and (len(text['symbol']) < 8):
+
+    if prevtimerCount == timerCount:
+        timer = threading.Timer(3, TimerPopped) # Call this same routine in first parameter seconds
+        timer.start()
+    
+    if stateUDP == NORMAL:
+        ip=get_ip()
+        if ip != UDP_IP_ADDRESS:
+            ReadUDP.terminate()
+            try:
+                serverSock.shutdown(socket.SHUT_RD)
+            except:
+                print("Error UDP shutdown")
+            
+            serverSock.close()
+            stateUDP = ISSUED_CLOSE
+            print(" IP changed")
+    else:
+        if stateUDP == ISSUED_CLOSE:
+            UDP_IP_ADDRESS = get_ip()
+            serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO)) 
+            ReadUDP =   ReadUDP_Port()
+            ReadUDPthread = threading.Thread(target=ReadUDP.run) 
+            ReadUDPthread.start()
+            stateUDP = NORMAL
+            print("restarted UDP thread")
+        
+        
+    
+    
+    prevtimerCount = timerCount
+    dtimer = threading.Timer(120, DeadManTimer) # 2 minutes
+    dtimer.start()
+
+
+
+
+
+timerCount = 1
+def TimerPopped():
+    global timerCount
+    global timer
+    global sName
+    keepGoing = True
+#    print ('refreshing')
+    try:
+        webSite = "https://api.iextrading.com/1.0/stock/" + sName + "/quote"
+    except:
+        keepGoing = False
+
+    if keepGoing:
+        try:
+            url = requests.get(webSite)
+        except:
+            keepGoing = False
+            
+    if (keepGoing and len(url.text) > 20)  and url.text[0]== '{': # 20 is arbitrary.  Here to catch invalid symbol
+            
+        text = json.loads(url.text)
+
+    #    labelLeftTop["text"] = "1234567\n45\n1234567"
+        quote = text['iexRealtimePrice']
         pClose = text['previousClose']
+        if not (isinstance(quote, int) or isinstance(quote, float)):
+            pClose = text['close']
+            quote = pClose
         chg = quote / pClose
         if chg < 1:
-            negPer = (1.0 - chg) * 100
-            if negPer < 100
-                if neg
-        else
-            posPer = (chg - 1.0) * 100
-        if (perct < 100):
-            if perct >= 10:
-            
-                
-                if 
-            
+            neg = True
+            percent = (1.0 - chg) * 100
+        else:
+            neg = False
+            percent = (chg - 1.0) * 100
+        if percent < 10:
+            sPercent = ("{:.2f}".format(round(percent,2)))
+        else:
+            if percent < 100:  
+                sPercent = ("{:.1f}".format(round(percent,1)))
+            else:
+                sPercent = "BIG"
+
+        # print (sPercent)
+
+        diff = abs(quote - pClose)
+        if diff < 1000:
+            sDiff = ("{:.2f}".format(round(diff,2)))
+        else:
+            if diff < 10000:
+                sDiff = ("{:.1f}".format(round(diff,1)))
+            else:
+                sDiff = "BIG"
+
+        if (neg == True):
+            labelRightTop["fg"] = "OrangeRed2"
+            labelLeftTop["fg"] = "OrangeRed2"
+            sPercent = "-" + str(sPercent) + "%"
+            sDiff = "-" + str(sDiff)
+        else:
+            labelRightTop["fg"] = "OliveDrab1"
+            labelLeftTop["fg"] = "OliveDrab1"
+            sPercent = "+" + str(sPercent) + "%"
+            sDiff = "+" + str(sDiff)
+        labelRightTop["bg"] = "#001F3F"
+
+
+
+
+
         
-        dlStr = text['symbol'] + "   "+"\n       " + "\n        "
-        labelLeftTop["text"] = dlStr
-    
-    timer = threading.Timer(2, TimerPopped) # Call this same routine in first parameter seconds
-    timer.start()
-TimerPopped()        
+        quoteStr = str(quote)
+    #    print (len(quoteStr))
+        if (len(quoteStr) > 1) and (len(quoteStr) < 8):
+            labelRightTop["text"] = quoteStr +  "\n" + sDiff + "\n" + sPercent     
+            dlStr = text['symbol']
+            labelLeftTop["text"] = dlStr + "\nCHANGE\n% CHG"
+    if not keepGoing:
+        
+        labelRightTop["bg"] = "purple1"
+        
+    if keepGoing:
+        timer = threading.Timer(3, TimerPopped) # Call this same routine in first parameter seconds
+        timer.start()
+        timerCount += 1
+        
+TimerPopped()
+
+#DeadManTimer()
+dtimer = threading.Timer(120, DeadManTimer) # 2 minutes
+dtimer.start()
+
 
         
 #def resize(event):
@@ -116,12 +259,89 @@ TimerPopped()
 labelLeftTop.bind("<Configure>", resize_lt)  #what about right top? 
 
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+print (get_ip())
+
+UDP_PORT_NO = 60066
+UDP_IP_ADDRESS = get_ip()
+
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+
+
+#while True:
+ #   data, addr = serverSock.recvfrom(1024)
+#    print ("Message: ", data)
+
+
+class ReadUDP_Port:
+    
+    def __init__(self):
+        self._running = True
+
+    def terminate(self):  
+        self._running = False  
+
+    def run(self):
+        global sName
+        while self._running:
+            print ("ReadUDP before recv")
+            data, addr = serverSock.recvfrom(256)
+            print ("Out of recvfrom")
+ #           serverSock.close()
+            print(data)
+            if len(data) > 0 and len(data) < 7:
+                sName = str(data, 'utf-8','ignore')
+                print(sName)
+        
+        print ("ReadUDP thread running")
+            
+ReadUDP =   ReadUDP_Port()
+ReadUDPthread = threading.Thread(target=ReadUDP.run) 
+ReadUDPthread.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def close():
     timer.cancel()
+    dtimer.cancel()
     win.destroy()
+    
+    ReadUDP.terminate()
+    serverSock.close()
 
 win.protocol("WM_DELETE_WINDOW", close)
+
+
+
+
+
+
 print ("bye")
 win.mainloop()
+print ("bye after  main")
 
 
--- marketmon/test.py
+++ marketmon/test.py
@@ -1,11 +1,177 @@
+import socket
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+print (get_ip())
+UDP_PORT_NO = 60066
+UDP_IP_ADDRESS = get_ip()
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+serverSock.close()
 
 
-print (round(80,1))
-print (101/96)
-print (str(round(((101/96-1)*100), 2)))
 
-print (100/95.05703422053232)
-string = (str(round(((100/95.05703422053232-1)*100), 2)))
-print (len(string))
-string2 =("{:.2f}".format(1.5))
-print (string2)
+
+
+
+
+##from threading import Thread
+##import time
+##
+##global cycle
+##cycle = 0.0
+##
+##class Hello5Program:  
+##    def __init__(self):
+##        self._running = True
+##
+##    def terminate(self):  
+##        self._running = False  
+##
+##    def run(self):
+##        global cycle
+##        while self._running:
+##            time.sleep(5) #Five second delay
+##            cycle = cycle + 1.0
+##            print ("5 Second Thread cycle+1.0 - ", cycle)
+##
+##class Hello2Program:  
+##    def __init__(self):
+##        self._running = True
+##
+##    def terminate(self):  
+##        self._running = False  
+##
+##    def run(self):
+##        global cycle
+##        while self._running:
+##            time.sleep(2) #Five second delay
+##            cycle = cycle + 0.5
+##            print ("5 Second Thread cycle+1.0 - ", cycle)
+##
+###Create Class
+##FiveSecond = Hello5Program()
+###Create Thread
+##FiveSecondThread = Thread(target=FiveSecond.run) 
+###Start Thread 
+##FiveSecondThread.start()
+##
+###Create Class
+##TwoSecond = Hello2Program()
+###Create Thread
+##TwoSecondThread = Thread(target=TwoSecond.run) 
+###Start Thread 
+##TwoSecondThread.start()
+##
+##
+##Exit = False #Exit flag
+##while Exit==False:
+## cycle = cycle + 0.1 
+## print ("Main Program increases cycle+0.1 - ", cycle)
+## time.sleep(1) #One second delay
+## if (cycle > 5): Exit = True #Exit Program
+##
+##TwoSecond.terminate()
+##FiveSecond.terminate()
+##print ("Goodbye :)")
+
+
+
+
+#UDP_IP = "192.168.1.22"
+#UDP_PORT = 5005
+#Message = "Hello, World"
+
+#print ("UDP target IP:", UDP_IP)
+#print ("UDP target port:", UDP_PORT)
+#print ("message:", Message)
+
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+#sock.sendto(b'Message' ,(UDP_IP, UDP_PORT))
+
+##UDP_IP = "192.168.1.168"
+##UDP_PORT = 5005
+##
+##sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+##sock.bind((UDP_IP, UDP_PORT))
+##
+##while True:
+##    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+##    print ("received message:", data)
+##
+##
+##
+##print (round(80,1))
+##print (101/96)
+##print (str(round(((101/96-1)*100), 2)))
+##
+##print (100/95.05703422053232)
+##string = (str(round(((100/95.05703422053232-1)*100), 2)))
+##print (len(string))
+##string2 =("{:.2f}".format(1.5))
+##print (string2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##quote = 51
+##pClose = 50
+##print ('======')
+##chg = quote / pClose
+##if chg < 1:
+##    neg = True
+##    percent = (1.0 - chg) * 100
+##else:
+##    neg = False
+##    percent = (chg - 1.0) * 100
+##if percent < 10:
+##    sPercent = ("{:.2f}".format(round(percent,2)))
+##else:
+##    if percent < 100:  
+##        sPercent = ("{:.1f}".format(round(percent,1)))
+##    else:
+##        sPercent = "BIG"
+##if (neg == True):
+##    sPercent = "-" + str(sPercent) + "%"
+##else:
+##    sPercent = "+" + str(sPercent) + "%"
+##    
+##print (sPercent)
+##diff = abs(quote - pClose)
+##if diff < 1000:
+##    sDiff = ("{:.2f}".format(round(diff,2)))
+##else:
+##    sDiff = "BIG"
+##print (sDiff)
+##
+##yo = 6
+##if isinstance(yo, int):
+##    print("ture")
+
+#chg = quote / pClose
+#TypeError: unsupported operand type(s) for /: 'NoneType' and 'float'
+    
+    
+
+        
