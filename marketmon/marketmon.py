8fadad1edb345538c9f4ffb2a9529937b5c1d902
Keith Arroyo <keitharroyo@gmail.com>
2019-01-30 18:52:39 -0600

    1-30-2019

--- marketmon/marketmon.py
+++ marketmon/marketmon.py
@@ -5,16 +5,17 @@ import tkinter.font  #gui
 import threading
 import socket
 
+
 print ('hello world')
 #url = requests.get('https://api.iextrading.com/1.0/stock/aapl/book')
-url = requests.get('https://api.iextrading.com/1.0/stock/aapl/quote')
+#url = requests.get('https://api.iextrading.com/1.0/stock/aapl/quote')
 
 # todo see what url.status_code is
-text = json.loads(url.text)
-print (text)
-print ('\n')
+#text = json.loads(url.text)
+#print (text)
+#print ('\n')
 #print (text['quote']['close'])
-print (text['close'])
+#print (text['close'])
 
 # start gui
 win = tki.Tk()
@@ -60,8 +61,10 @@ win.grid_rowconfigure(0,weight=1)
 #print (tkinter.font.families())
 #print (tkinter.font.names())
 
-labelLeftTop["text"] = "1234567\n45\n1234567"
-labelRightTop["text"] = "1234567\n45\n1234567"
+labelLeftTop["text"] = "\nUDPport\nticker"
+labelRightTop["text"] = "\n60066\n<Enter>"
+firstTimeThrough = True
+
 oldHeight = 0
 def resize_lt(event):
     print("In lt",event.height, event.x, event.y)
@@ -87,16 +90,20 @@ stockName = ""
 def keystroke(event):
     global stockName
     global sName
+    global firstTimeThrough
     if len(event.char) == 1:
         
         print (ord(event.char))
         if ord(event.char) == 13:
             print ("Enter Key pressed")
             print (stockName)
-            sName = stockName
+            if len(stockName):
+                if Checkname(stockName):
+                    sName = stockName
+                    firstTimeThrough = False
             stockName = ""
         else:
-            if len(stockName) < 6:
+            if len(stockName) < 7:
                 stockName += event.char
 
 win.bind("<Key>", keystroke)
@@ -105,52 +112,52 @@ NORMAL = 0
 ISSUED_CLOSE = 1
 stateUDP = NORMAL
 
-prevtimerCount = 0
-def DeadManTimer():
-    global dtimer
-    global timer
-    global prevtimerCount
-    global  serverSock
-    global ReadUDP
-    global UDP_IP_ADDRESS
-    global stateUDP
-    global ISSUED_CLOSE
-    global NORMAL
-    
-
-    if prevtimerCount == timerCount:
-        timer = threading.Timer(3, TimerPopped) # Call this same routine in first parameter seconds
-        timer.start()
-    
-    if stateUDP == NORMAL:
-        ip=get_ip()
-        if ip != UDP_IP_ADDRESS:
-            ReadUDP.terminate()
-            try:
-                serverSock.shutdown(socket.SHUT_RD)
-            except:
-                print("Error UDP shutdown")
-            
-            serverSock.close()
-            stateUDP = ISSUED_CLOSE
-            print(" IP changed")
-    else:
-        if stateUDP == ISSUED_CLOSE:
-            UDP_IP_ADDRESS = get_ip()
-            serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO)) 
-            ReadUDP =   ReadUDP_Port()
-            ReadUDPthread = threading.Thread(target=ReadUDP.run) 
-            ReadUDPthread.start()
-            stateUDP = NORMAL
-            print("restarted UDP thread")
-        
-        
-    
-    
-    prevtimerCount = timerCount
-    dtimer = threading.Timer(120, DeadManTimer) # 2 minutes
-    dtimer.start()
+##prevtimerCount = 0
+##def DeadManTimer():
+##    global dtimer
+##    global timer
+##    global prevtimerCount
+##    global  serverSock
+##    global ReadUDP
+##    global UDP_IP_ADDRESS
+##    global stateUDP
+##    global ISSUED_CLOSE
+##    global NORMAL
+##    
+##
+##    if prevtimerCount == timerCount:
+##        timer = threading.Timer(3, TimerPopped) # Call this same routine in first parameter seconds
+##        timer.start()
+##    
+##    if stateUDP == NORMAL:
+##        ip=get_ip()
+##        if ip != UDP_IP_ADDRESS:
+##            ReadUDP.terminate()
+##            try:
+##                serverSock.shutdown(socket.SHUT_RD)
+##            except:
+##                print("Error UDP shutdown")
+##            
+##            serverSock.close()
+##            stateUDP = ISSUED_CLOSE
+##            print(" IP changed")
+##    else:
+##        if stateUDP == ISSUED_CLOSE:
+##            UDP_IP_ADDRESS = get_ip()
+##            serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+##            serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO)) 
+##            ReadUDP =   ReadUDP_Port()
+##            ReadUDPthread = threading.Thread(target=ReadUDP.run) 
+##            ReadUDPthread.start()
+##            stateUDP = NORMAL
+##            print("restarted UDP thread")
+##        
+##        
+##    
+##    
+##    prevtimerCount = timerCount
+##    dtimer = threading.Timer(120, DeadManTimer) # 2 minutes
+##    dtimer.start()
 
 
 
@@ -161,92 +168,139 @@ def TimerPopped():
     global timerCount
     global timer
     global sName
-    keepGoing = True
-#    print ('refreshing')
-    try:
-        webSite = "https://api.iextrading.com/1.0/stock/" + sName + "/quote"
-    except:
-        keepGoing = False
+    
+    global serverSock
+    global ReadUDP
+    global UDP_IP_ADDRESS
+    global stateUDP
+    global ISSUED_CLOSE
+    global NORMAL
 
-    if keepGoing:
+    global firstTimeThrough
+
+    if not firstTimeThrough:
+
+    
+        keepGoing = True
+    #    print ('refreshing')
+        
+        webSite = "https://api.iextrading.com/1.0/stock/" + sName + "/quote"
+                
         try:
-            url = requests.get(webSite)
+            url = requests.get(webSite,timeout=7)
         except:
             keepGoing = False
-            
-    if (keepGoing and len(url.text) > 20)  and url.text[0]== '{': # 20 is arbitrary.  Here to catch invalid symbol
-            
-        text = json.loads(url.text)
+#            print("requests.get failed")
+                
+        if (keepGoing and (len(url.text) > 20)  and (url.text[0]== '{')): # 20 is arbitrary.  Here to catch invalid symbol
+                
+            text = json.loads(url.text)
 
-    #    labelLeftTop["text"] = "1234567\n45\n1234567"
-        quote = text['iexRealtimePrice']
-        pClose = text['previousClose']
-        if not (isinstance(quote, int) or isinstance(quote, float)):
-            pClose = text['close']
-            quote = pClose
-        chg = quote / pClose
-        if chg < 1:
-            neg = True
-            percent = (1.0 - chg) * 100
-        else:
-            neg = False
-            percent = (chg - 1.0) * 100
-        if percent < 10:
-            sPercent = ("{:.2f}".format(round(percent,2)))
-        else:
-            if percent < 100:  
-                sPercent = ("{:.1f}".format(round(percent,1)))
+        #    labelLeftTop["text"] = "1234567\n45\n1234567"
+            quote = text['iexRealtimePrice']
+            pClose = text['previousClose']
+            if (not (isinstance(quote, int) or isinstance(quote, float))) or quote == 0:
+                pClose = text['close']
+                quote = pClose
+            chg = quote / pClose
+            if chg < 1:
+                neg = True
+                percent = (1.0 - chg) * 100
             else:
-                sPercent = "BIG"
-
-        # print (sPercent)
-
-        diff = abs(quote - pClose)
-        if diff < 1000:
-            sDiff = ("{:.2f}".format(round(diff,2)))
-        else:
-            if diff < 10000:
-                sDiff = ("{:.1f}".format(round(diff,1)))
+                neg = False
+                percent = (chg - 1.0) * 100
+            if percent < 10:
+                sPercent = ("{:.2f}".format(round(percent,2)))
             else:
-                sDiff = "BIG"
+                if percent < 100:  
+                    sPercent = ("{:.1f}".format(round(percent,1)))
+                else:
+                    sPercent = "BIG"
 
-        if (neg == True):
-            labelRightTop["fg"] = "OrangeRed2"
-            labelLeftTop["fg"] = "OrangeRed2"
-            sPercent = "-" + str(sPercent) + "%"
-            sDiff = "-" + str(sDiff)
+            # print (sPercent)
+
+            diff = abs(quote - pClose)
+            if diff < 1000:
+                sDiff = ("{:.2f}".format(round(diff,2)))
+            else:
+                if diff < 10000:
+                    sDiff = ("{:.1f}".format(round(diff,1)))
+                else:
+                    sDiff = "BIG"
+
+            if (neg == True):
+                labelRightTop["fg"] = "OrangeRed2"
+                labelLeftTop["fg"] = "OrangeRed2"
+                sPercent = "-" + str(sPercent) + "%"
+                sDiff = "-" + str(sDiff)
+            else:
+                labelRightTop["fg"] = "OliveDrab1"
+                labelLeftTop["fg"] = "OliveDrab1"
+                sPercent = "+" + str(sPercent) + "%"
+                sDiff = "+" + str(sDiff)
+            labelRightTop["bg"] = "#001F3F"
+            labelLeftTop["bg"] = "#001F3F"
+
+
+
+            
+            quoteStr = str(quote)
+        #    print (len(quoteStr))
+            if (len(quoteStr) > 1) and (len(quoteStr) < 8):
+                labelRightTop["text"] = quoteStr +  "\n" + sDiff + "\n" + sPercent     
+                dlStr = text['symbol']
+                labelLeftTop["text"] = dlStr + "\nCHANGE\n% CHG"
         else:
-            labelRightTop["fg"] = "OliveDrab1"
-            labelLeftTop["fg"] = "OliveDrab1"
-            sPercent = "+" + str(sPercent) + "%"
-            sDiff = "+" + str(sDiff)
-        labelRightTop["bg"] = "#001F3F"
-
-
-
-
-
-        
-        quoteStr = str(quote)
-    #    print (len(quoteStr))
-        if (len(quoteStr) > 1) and (len(quoteStr) < 8):
-            labelRightTop["text"] = quoteStr +  "\n" + sDiff + "\n" + sPercent     
-            dlStr = text['symbol']
+            labelLeftTop["bg"] = "purple1"
+            dlStr = sName
             labelLeftTop["text"] = dlStr + "\nCHANGE\n% CHG"
-    if not keepGoing:
+            labelRightTop["text"] = "0\n0\n0"
+            
         
-        labelRightTop["bg"] = "purple1"
+
+
+                
+        if not keepGoing:
+            
+            labelRightTop["bg"] = "purple1"
+
+            
+    if timerCount == 3 or stateUDP != NORMAL:
+            timerCount = 0
+            if stateUDP == NORMAL:
+                ip=get_ip()
+                if ip != UDP_IP_ADDRESS:
+                    ReadUDP.terminate()
+                    try:
+                        serverSock.shutdown(socket.SHUT_RD)
+                    except:
+                        print("Error UDP shutdown")
+                
+                    serverSock.close()
+                    stateUDP = ISSUED_CLOSE
+                    print(" IP changed")
+            else:
+                if stateUDP == ISSUED_CLOSE:
+                    UDP_IP_ADDRESS = get_ip()
+                    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO)) 
+                    ReadUDP =   ReadUDP_Port()
+                    ReadUDPthread = threading.Thread(target=ReadUDP.run) 
+                    ReadUDPthread.start()
+                    stateUDP = NORMAL
+                    print("restarted UDP thread") 
+
+            
+    
+    timer = threading.Timer(3, TimerPopped) # Call this same routine in first parameter seconds
+    timer.start()
+    timerCount += 1
         
-    if keepGoing:
-        timer = threading.Timer(3, TimerPopped) # Call this same routine in first parameter seconds
-        timer.start()
-        timerCount += 1
-        
-TimerPopped()
+
 
 #DeadManTimer()
-dtimer = threading.Timer(120, DeadManTimer) # 2 minutes
-dtimer.start()
+#dtimer = threading.Timer(120, DeadManTimer) # 2 minutes
+#dtimer.start()
 
 
         
@@ -282,8 +336,13 @@ serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 #while True:
  #   data, addr = serverSock.recvfrom(1024)
 #    print ("Message: ", data)
-
-
+def Checkname(s):
+    for char in s:
+        if (char < 'a' or char > 'z') and (char < 'A' or char > 'Z') and char != '.':
+            return False
+    return True
+        
+        
 class ReadUDP_Port:
     
     def __init__(self):
@@ -294,6 +353,7 @@ class ReadUDP_Port:
 
     def run(self):
         global sName
+        global firstTimeThrough
         while self._running:
             print ("ReadUDP before recv")
             data, addr = serverSock.recvfrom(256)
@@ -301,7 +361,9 @@ class ReadUDP_Port:
  #           serverSock.close()
             print(data)
             if len(data) > 0 and len(data) < 7:
-                sName = str(data, 'utf-8','ignore')
+                if Checkname(str(data, 'utf-8','ignore')):
+                    sName = str(data, 'utf-8','ignore')
+                    firstTimeThrough = False
                 print(sName)
         
         print ("ReadUDP thread running")
@@ -311,7 +373,7 @@ ReadUDPthread = threading.Thread(target=ReadUDP.run)
 ReadUDPthread.start()
 
 
-
+TimerPopped()
 
 
 
@@ -327,7 +389,7 @@ ReadUDPthread.start()
 
 def close():
     timer.cancel()
-    dtimer.cancel()
+#    dtimer.cancel()
     win.destroy()
     
     ReadUDP.terminate()
